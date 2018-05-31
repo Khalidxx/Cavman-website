1a027a604aa9c241ff1f76407fdaae40d6aae35e
Khalidxx <FXTRyio@gmail.com>
2018-05-31 19:54:05 +0800

    sample horizontal scroll

--- src/JS/Portfolio.js
+++ src/JS/Portfolio.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
+import HorizontalScroll from 'react-scroll-horizontal'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 
-import '../CSS/main.css';
+
+import '../CSS/portfolio.css';
 
 export default class Portfolio extends Component {
     render() {
+        const child = { width: `30em`, height: `100%` }
         return (
-            <div className='a1'>
-                <ReactCSSTransitionGroup
-                    key={1}
-                    transitionName="scrl"
-                    transitionAppear={true}
-                    transitionAppearTimeout={1000}
-                    transitionEnter={false}
-                    transitionLeave={false}>
-                    <h1 className='hello'>This is the Portfolio</h1>
-                </ReactCSSTransitionGroup>
-                <Button type='primary'> Hello </Button>
-                <Button> Goodbye </Button>
+            <div className='horizontal-scroller'>
+                <div className='card' style={{ height: '90vh', width: '1700px', backgroundColor: 'red' }}>xyz</div>
+                <div className='card' style={{ height: '90vh', width: '1700px', backgroundColor: 'blue' }}>xyz</div>
+                <div className='card' style={{ height: '90vh', width: '1700px', backgroundColor: 'black' }}>xyz</div>
+                <div className='card' style={{ height: '90vh', width: '1700px', backgroundColor: 'green' }}>xyz</div>
+                <div className='card' style={{ height: '90vh', width: '1700px', backgroundColor: 'yellow' }}>xyz</div>
+                <div className='card' style={{ height: '90vh', width: '1700px', backgroundColor: 'aqua' }}>xyz</div>
+                <div className='card' style={{ height: '90vh', width: '1700px', backgroundColor: 'brown' }}>xyz</div>
+                <div className='card' style={{ height: '90vh', width: '1700px', backgroundColor: 'magenta' }}>xyz</div>
             </div>
         );
     }
